c0e8a0eea4da33b23bb1f7131d04da546d6feb3d
Ankit <ankit.sangode@gmail.com>
2025-07-15 05:33:00 +0530

    data ingestion completed

---
+++ README.md
@@ -0,0 +1,9 @@
+1. update config.yaml
+2. update secrets.yaml
+3. update params.yaml
+4. update the entity
+5. update configuration manager in src config
+6. update components
+7. update the pipeline
+8. update the main.py
+9. update the dvc.yaml
---
+++ srcCNN_Classifier/pipeline/stage_01_data_ingestion.py
@@ -0,0 +1,27 @@
+from srcCNN_Classifier.config.configuration import configurationManager
+from srcCNN_Classifier.components.data_ingestion import DataIngestion
+from srcCNN_Classifier.logger import logger
+
+STAGE_NAME= "Data Ingestion stage"
+
+class DataIngestionTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = configurationManager()
+        data_ingestion_config = config.get_data_ingestion_config()
+        data_ingestion = DataIngestion(config=data_ingestion_config)
+        data_ingestion.download_file()
+        data_ingestion.extract_zip_file()
+
+
+if __name__ == "__main__":
+    try:
+        logger.info(f">>>>> Stage {STAGE_NAME} started <<<<<")
+        obj = DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> Stage {STAGE_NAME} completed <<<<<<<<<")
+    except Exception as e:
+        logger.exception(e)
+        raise e
