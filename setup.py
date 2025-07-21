641aea3f22824b94c1d668bfebbb309df2fb574a
Ankit <ankit.sangode@gmail.com>
2025-07-21 16:59:46 +0530

    Structure added

--- setup.py
+++ setup.py
@@ -1,23 +1,29 @@
-from setuptools import find_packages,setup
+import setuptools
 
-with open("README.md",'r',encoding='utf-8') as f:
+with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
-def get_requirements(file_path:str)->list[str]:
-    requirements = []
-    with open(file_path) as file_obj:
-        requirements = file_obj.readlines()
-        requirements = [req.replace("\n","") for req in requirements]
 
-        if "-e ." in requirements:
-            requirements.remove("-e .")
-    return requirements
+__version__ = "0.0.0"
 
-setup(
-    name="CNNClassifier",
-    version="0.0.1",
-    author="Ankit",
-    author_email="Ankitsangode0911@gmail.com",
-    packages=find_packages(),
-    install_requires=get_requirements("requirements.txt")
+REPO_NAME = "Chicken-Disease-Classification--Project"
+AUTHOR_USER_NAME = "Ankit"
+SRC_REPO = "cnnClassifier"
+AUTHOR_EMAIL = "ankit.sangode0911@gmail.com"
+
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small python package for CNN app",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
 )
