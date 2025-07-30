2c52f1798cbc74bdd8fc374e3d45e5826605461b
Ankit <ankit.sangode@gmail.com>
2025-07-30 19:20:30 +0530

    Pipelines ready

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from CNN_Classifier import logger
 from CNN_Classifier.pipeline.stage_01_data_ingestion import DataIngestionPipeline
 from CNN_Classifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
-
+from CNN_Classifier.pipeline.stage_03_training import ModelTrainingPipeline
+from CNN_Classifier.pipeline.stage_04_evaluation import EvaluationPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
 
@@ -26,3 +27,25 @@ except Exception as e:
     logger.exception(e)
     raise e
 
+STAGE_NAME = "Training"
+
+try:
+    logger.info(f"*****************")
+    logger.info(f">>>>>>> Stage {STAGE_NAME} started <<<<<<<<<<<")
+    obj = ModelTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Evaluation"
+
+try:
+    logger.info(f"************")
+    logger.info(f">>>>>>>>> stage {STAGE_NAME} started <<<<<<<<<<<<")
+    obj = EvaluationPipeline()
+    obj.main()
+    logger.info(f"<<<<<<<<<< stage {STAGE_NAME} completed")
+except Exception as e:
+    raise e
